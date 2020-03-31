e36ca77aebc2f24c557f52307659a08652e4f01b
Shane Knudsen <shaneknu@gmail.com>
2020-03-31 16:07:36 -0400

    Created paste event listener to swap out newlines for </p><p>

--- editor/editorDirective.js
+++ editor/editorDirective.js
@@ -9,7 +9,20 @@
       restrict: 'E',
       templateUrl: '/editor/editor.html',
       link: function(scope, elem, attrs) {
+        // Set all enter key presses to be new paragraphs
         document.execCommand("defaultParagraphSeparator", false, "p");
+
+        const editDiv = elem[0].querySelector('div[contentEditable="true"]');
+
+        editDiv.addEventListener('paste', evt => {
+          const text = (evt.clipboardData || window.clipboardData)
+            .getData('text')
+            .replace(/\n/g, '</p><p>');
+
+          document.execCommand('insertHTML', false, text);
+
+          evt.preventDefault();
+        });
 
         scope.modifyDoc = modifyDoc;
         scope.insertComment = insertComment;
