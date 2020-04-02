01689915493760e7dcef6d66469c1c78f02e21e0
Shane Knudsen <shaneknu@gmail.com>
2020-04-02 10:22:24 -0400

    converted to CRLF

--- editor/editorDirective.js
+++ editor/editorDirective.js
@@ -1,5 +1,6 @@
 (function() {
-  "use strict";
+
+  'use strict';
 
   angular.module('editorTest').directive('appEditor', ['$sanitize', function($sanitize) {
     return {
@@ -10,29 +11,29 @@
       templateUrl: '/editor/editor.html',
       link: function(scope, elem, attrs) {
         // Set all enter key presses to be new paragraphs
-        document.execCommand("defaultParagraphSeparator", false, "p");
+        document.execCommand('defaultParagraphSeparator', false, 'p');
 
         const DEFAULT_TEXT = '<p>Add text...</p>';
         const editor = elem[0].querySelector('div[contenteditable]');
+
         editor.innerHTML = scope.text;
         if (editor.innerHTML === '' || editor.innerHTML === '<p></p>') {
           editor.innerHTML = DEFAULT_TEXT;
         }
-
 
         scope.modifyDoc = modifyDoc;
         scope.insertComment = insertComment;
         scope.handleFocus = handleFocus;
         scope.handleBlur = handleBlur;
         scope.handleInput = handleInput;
-        
+        scope.handlePaste = handlePaste;
+
         function modifyDoc(command, value) {
-          document.execCommand(command, false, value)
-        };
+          document.execCommand(command, false, value);
+        }
 
         function insertComment() {
           const selectedText = document.getSelection().toString();
-
           document.execCommand('insertHTML', false, `<mark>${selectedText}</mark>`);
         }
 
@@ -44,7 +45,6 @@
         }
 
         function handleBlur() {
-          console.log(editor.innerHTML);
           if (editor.innerHTML === '' || editor.innerHTML === '<p></p>' || editor.innerHTML === '<p><br></p>') {
             editor.innerHTML = '';
             document.execCommand('insertHTML', false, DEFAULT_TEXT);
@@ -59,7 +59,11 @@
             scope.onsave({ value: $sanitize(editor.innerHTML) });
           }, 3000);
         }
+
+        function handlePaste(evt) {
+          console.log(evt)
+        }
       }
     };
   }]);
-})()
+})();
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-"use strict";
-const module = angular.module('editorTest',['ngSanitize']);
+'use strict';
+angular.module('editorTest',['ngSanitize'])
+  .controller('main', ['$sce', function($sce) {
+    const model = this;
 
-module.controller('main', ['$sce', function($sce) {
-  const model = this;
+    model.txt1 = $sce.trustAsHtml('<p>foo</p>');
+    model.txt2 = $sce.trustAsHtml('<p></p>');
+    
+    model.saveHandler = saveHandler;
 
-  model.txt1 = $sce.trustAsHtml('<p>foo</p>');
-  model.txt2 = $sce.trustAsHtml('<p></p>');
-  
-  model.saveHandler = saveHandler;
-
-  function saveHandler() {
-    console.log('saveHandler', arguments)
-  }
-}]);
+    function saveHandler() {
+      console.log('saveHandler', arguments);
+    }
+  }]);
